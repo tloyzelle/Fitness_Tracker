949c4f318f750cdcf0e57854e032563a71fa792b
Taegan Loyzelle <tloyzelle@gmail.com>
2021-08-26 12:18:12 -0400

    adding server.js base code

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.listen(PORT,function(){
+    console.log(`App listening on Port ${PORT}`);
+});
