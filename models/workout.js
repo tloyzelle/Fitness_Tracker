e83a7a5598d055295b5f60c8dd3c3ed4c8f7a130
Taegan Loyzelle <tloyzelle@gmail.com>
2021-08-26 12:56:43 -0400

    adding workout.js

---
+++ models/workout.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: () => new Date()
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Enter exercise name"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Enter exercise name"
+                },
+                duration: {
+                    type: Number,
+                    required: "Enter exercise length in minutes"
+                },
+                weight: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                distance: {
+                    type: Number
+                }
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+workoutSchema.virtual("totalDuration").get(function () {
+    return this.exercise.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = workout;
