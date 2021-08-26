243b6217f70ae6bcf13b2d5803f04232da1343a0
Taegan Loyzelle <tloyzelle@gmail.com>
2021-08-26 12:33:31 -0400

    adding api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,54 @@
+const workout = require("../models/workout");
+
+module.exports = function(app){
+    app.get("/api/workouts", function(req,res){
+        workout.find()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+    });
+
+    app.get("/api/workouts", function(req,res){
+        workout.create({})
+        .then(data => res.json(data))
+        .catch(err => {
+            res.json(err)
+        })
+    });
+
+    app.get("/api/workouts/range", function(req,res){
+        workout.find()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+    });
+
+    app.post("/api/workouts/range", function(req,res){
+        workout.create({})
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+    });
+
+    app.put("/api/workouts/:id", ({body,params},res) => {
+        workout.findByIdAndUpdate(
+            params.id,
+            {$push:{exercises:body}},
+            {new: true,runValidators:true}
+        )
+        .then(data => res.json(data))
+        .cath(err => {
+            res.json(err)
+        })
+    });
+
+}
